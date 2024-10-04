367bfa29b1844d4a1056e4a03488f7fa3d1fee6f
nxrlyj <e0773273@u.nus.edu>
2024-10-04 16:22:03 +0800

    Update svm.py

--- svm/svm.py
+++ svm/svm.py
@@ -4,7 +4,6 @@ import math
 import random
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold, cross_val_score
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, f1_score
@@ -20,7 +19,6 @@ y_train = train[['label']]
 X_test = test.iloc[:, :-2]
 y_test = test[['label']]
 
-
 #%%
 svc = SVC()
 svc.fit(X_train, y_train)
@@ -29,3 +27,4 @@ print(accuracy_score(y_train, svc.predict(X_train))) # 0.9716750354062057
 
 print(f1_score(y_test, svc.predict(X_test), average='weighted')) # 0.9360546876940178
 print(f1_score(y_train, svc.predict(X_train), average='weighted')) # 0.971551119209504
+
