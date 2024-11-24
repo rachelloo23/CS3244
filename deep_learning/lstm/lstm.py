38f23eefff24fee9ee3bbd9a5eb77cd77228f556
QJ <bqjun0000@gmail.com>
2024-11-24 16:48:21 +0800

    Modify model class for loadability

--- deep_learning/lstm/lstm.py
+++ deep_learning/lstm/lstm.py
@@ -12,23 +12,27 @@ class LSTMModel(tf.keras.Model):
     - Dropout for regularization.
     - An output Dense layer with softmax activation for classification.
     """
-    def __init__(self, num_classes, lstm_units, dropout_rate):
+    def __init__(self, num_classes, lstm_units, dropout_rate, **kwargs):
         """
-        Initialize the LSTMModel.
+        Initialize the LSTMModel with the ability to handle additional keyword arguments
+        that are passed by Keras during model deserialization.
 
         Parameters:
         - num_classes (int): The number of output classes.
         - lstm_units (int): The number of units in the LSTM layer.
         - dropout_rate (float): The dropout rate for regularization.
         """
-        super().__init__()
+        super(LSTMModel, self).__init__(**kwargs)  # Pass any additional kwargs to the superclass constructor
+        self.num_classes = num_classes
+        self.lstm_units = lstm_units
+        self.dropout_rate = dropout_rate
         self.masking = tf.keras.layers.Masking(mask_value=0.0)
         self.layer_norm = tf.keras.layers.LayerNormalization()
         self.lstm = tf.keras.layers.LSTM(lstm_units)
         self.dense = tf.keras.layers.Dense(lstm_units // 2, activation='relu')
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.fc = tf.keras.layers.Dense(num_classes, activation='softmax')
-            
+
     def call(self, inputs):
         """
         Forward pass of the model.
@@ -44,5 +48,23 @@ class LSTMModel(tf.keras.Model):
         x = self.lstm(x)
         x = self.dense(x)
         x = self.dropout(x)
-        output = self.fc(x)
-        return output
+        return self.fc(x)
+    
+    def get_config(self):
+        """
+        Returns the configuration of the model for saving purposes.
+        """
+        config = super().get_config()
+        config.update({
+            'num_classes': self.num_classes,
+            'lstm_units': self.lstm_units,
+            'dropout_rate': self.dropout_rate
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        """
+        Instantiates a model from its configuration (output of get_config).
+        """
+        return cls(**config)
